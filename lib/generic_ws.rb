50feb4f54c20ee55de2137ac65a646645bba3451
Michał Ajduk <majduk@gmail.com>
2015-12-16 12:11:39 +0100

    Update generic_ws.rb

--- lib/generic_ws.rb
+++ lib/generic_ws.rb
@@ -7,25 +7,33 @@ class GenericWsClient < ServiceProxy::Base
     return ((not config.blank?) and (not config[:wsdl].blank?))
   end
 
+  def initialize
+      name=self.class.config_name
+      @config=self.class.load_config name
+      raise Exception.new "Unconfigured: #{name}" if @config.blank?
+      super( @config[:wsdl] ) unless @config[:wsdl].blank?
+      if self.class.enabled?
+        self.http.read_timeout = 20
+        self.http.read_timeout = @config[:read_timeout] unless @config[:read_timeout].blank?
+      end
+      @no_cache=( @config[:no_cache]==true )
+      @cache_validity=30.seconds
+      @cache_validity='#{config[:cache_validity_seconds]'.to_i.seconds unless @config[:cache_validity_seconds].blank?
+  end
+
+protected
+  def self.load_config name
+    return APP_CONFIG[name]
+  end
+
   def self.config
-    return APP_CONFIG[config_name]
+    name=config_name
+    return APP_CONFIG[name]
   end
 
   def self.config_name
-    return "#{self}".underscore.to_sym
-  end
-
-  def initialize
-      @config=self.class.config   
-      raise Exception.new "Unconfigured: #{self.class.config_name}" if @config.blank?   
-      super( @config[:wsdl] ) unless @config[:wsdl].blank?
-      if self.class.enabled? 
-        self.http.read_timeout = 20
-        self.http.read_timeout = @config[:read_timeout] unless @config[:read_timeout].blank?
-      end       
-      @no_cache=( @config[:no_cache]==true )
-      @cache_validity=30.seconds
-      @cache_validity='#{config[:cache_validity_seconds]'.to_i.seconds unless @config[:cache_validity_seconds].blank?         
+    name="#{self}".underscore.to_sym
+    return name
   end
 
 end
